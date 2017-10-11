21db5072884985730e8965a5113e0f8ea9f8dca5
angulostephanie <angulostephanie21@gmail.com>
2017-10-11 13:49:53 -0700

    Create test data from the test data, implemented a very brute forced version of something that mimicks hill climbing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,25 @@ import com.opencsv.CSVReaderBuilder;
 import com.opencsv.CSVReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+//        List<Professor> profsList = getProfessors("/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/advisorMatch/professors.csv");
+//        List<Student> studentList = getStudents("/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/advisorMatch/students.csv");
+//        for(int i = 0; i < studentList.size(); i++)
+//            System.out.println(studentList.get(i).majors);
+//        System.out.println("-----------------");
+//        for(int i = 0; i < profsList.size(); i++)
+//            System.out.println(profsList.get(i).department);
+
+        List<Student> studentList = getStudents("/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/advisorMatch/teststudent.csv");
+        List<Professor> profsList = getProfessors("/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/advisorMatch/testprofessor.csv");
+        hillClimbing(profsList, studentList);
+    }
+    public static List getProfessors(String fileName) {
         try {
-            CSVReader reader = new CSVReaderBuilder(new FileReader("/Users/stephanieangulo/Desktop/cs-stuff/fall-2017/advisorMatch/professors.csv")).withSkipLines(1).build();
+            CSVReader reader = new CSVReaderBuilder(new FileReader(fileName)).withSkipLines(1).build();
             List<Professor> profsList = new ArrayList<Professor>();
             List<String[]> infoList = reader.readAll();
             Iterator<String[]> iterator = infoList.iterator();
@@ -24,14 +35,76 @@ public class Main {
                 prof.department = info[4];
                 profsList.add(prof);
             }
-
-            System.out.println(profsList);
-
+            Collections.shuffle(profsList);
+            return profsList;
         } catch(IOException e) {
             e.printStackTrace();
         }
+        return null;
+    }
+    public static List getStudents(String fileName) {
+        try {
+            CSVReader reader = new CSVReaderBuilder(new FileReader(fileName)).withSkipLines(1).build();
+            List<Student> studentList = new ArrayList<Student>();
+            List<String[]> infoList = reader.readAll();
+            Iterator<String[]> iterator = infoList.iterator();
+
+            while(iterator.hasNext()) {
+                String[] info = iterator.next();
+                Student student = new Student();
+
+                student.id = info[1];
+                student.status = info[2];
+                student.last = info[4];
+                student.first = info[3];
+                student.majors = info[7];
+                student.minors = info[8];
+                studentList.add(student);
+            }
+            Collections.shuffle(studentList);
+            return studentList;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
+    public static void hillClimbing(List<Professor> professors, List<Student> students) {
+        int generation = 0;
+        Map<Student, Professor> studentProfMatches = new HashMap<Student, Professor>();
+
+        while(generation < 100) {
+//            int fitness = 0;
+            for(int i = 0; i < students.size(); i++) {
+                int maxMatch = 0;
+                Professor bestProf = new Professor();
+//                Student unmatchedStudent = new Student();
+                for(int j = 0; j < professors.size(); j++) {
+                    int currentMatch = getScore(professors.get(j), students.get(i));
+                    if(currentMatch > maxMatch) {
+                        maxMatch = currentMatch;
+                        bestProf = professors.get(j);
+                    }
+                }
+//                if(unmatchedStudent != null) {
+//                    if(studentProfMatches.containsValue())
+//                }
+                studentProfMatches.put(students.get(i), bestProf);
+            }
+            generation++;
+        }
+        for (Map.Entry<Student, Professor> entry : studentProfMatches.entrySet()) {
+            System.out.println(entry.getKey().majors + " : " + entry.getValue().department);
+        }
 
     }
+    public static int getScore(Professor prof, Student student) {
+        int score = 0;
+        if(prof.department.equalsIgnoreCase(student.majors))
+            score++;
+        return score;
+    }
+
+
 }
---
+++ src/main/java/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    String id, status, last, first, majors, minors;
+
+    public Student() {
+        this.id = id;
+        this.status = status;
+        this.last = last;
+        this.first = first;
+        this.majors = majors;
+        this.minors = minors;
+    }
+}
