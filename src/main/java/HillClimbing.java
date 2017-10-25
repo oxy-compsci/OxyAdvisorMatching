859bb03540fee329731d1e9e8504223e286034ef
angulostephanie <angulostephanie21@gmail.com>
2017-10-25 14:32:54 -0700

    Added swapping

--- src/main/java/HillClimbing.java
+++ src/main/java/HillClimbing.java
@@ -1,17 +1,33 @@
 import java.util.*;
 
 public class HillClimbing {
-    public static final int MAX_ITERATIONS = 150;
+    public static final int MAX_ITERATIONS = 300;
 
     public Map<Student, Professor> findBestMatches(List<Student> students, List<Professor> professors) {
         int generation = 0;
         Map<Student, Professor> currentMap = createMap(students, professors);
+        Map<Student, Professor> bestNextMap = null;
         while(generation < MAX_ITERATIONS) {
-            Map<Student, Professor> nextMap = createMap(students, professors);
+            Map<Student, Professor> nextMap = null;
+            int bestScore = -1;
+            int nextScore = -1;
+            for (int swap = 0; swap < 100; swap++) {
+                nextMap =  (HashMap<Student,Professor>)((HashMap<Student,Professor>)currentMap).clone();
+                Random random= new Random();
+                List<Student>keys = new ArrayList<Student>(currentMap.keySet());
+                Student randomKey1 = keys.get(random.nextInt(keys.size()));
+                Student randomKey2 = keys.get(random.nextInt(keys.size()));
+                nextMap.put(randomKey1, currentMap.get(randomKey2));
+                nextMap.put(randomKey2, currentMap.get(randomKey1));
+                nextScore = score(nextMap);
+                if (bestNextMap == null || nextScore > bestScore) {
+                    bestNextMap = nextMap;
+                    bestScore = score(nextMap);
+                }
+            }
             int currentScore = score(currentMap);
-            int nextScore = score(nextMap);
             System.out.println("Generation #" + (generation+1));
-            System.out.println("Current score: " + currentScore);
+            System.out.println("Current best score: " + bestScore);
             System.out.println("Next score: " + nextScore);
             System.out.println("-----------------------");
             if(nextScore > currentScore) {
@@ -20,9 +36,8 @@ public class HillClimbing {
             }
             generation++;
         }
-        double successRate = ((double)score(currentMap)/students.size())*100.0;
-        System.out.println("Success rate: " + successRate);
-        return currentMap;
+
+        return bestNextMap;
     }
 
     public int score(Map<Student, Professor> map) {
